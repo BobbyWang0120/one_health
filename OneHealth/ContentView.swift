05e2707275ee0496714378496dc5f337f76a1f9b
Bobby Wang <yyaustin@bu.edu>
2024-12-26 17:04:48 -0500

    Initial Commit

---
+++ OneHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneHealth
+//
+//  Created by Bobby Wang on 12/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneHealth/OneHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHealthApp.swift
+//  OneHealth
+//
+//  Created by Bobby Wang on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct OneHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
